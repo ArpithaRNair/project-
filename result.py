26192c83936104e7e7fe917444f4bd0c283ffcdc
ArpithaRNair <86906851+ArpithaRNair@users.noreply.github.com>
2021-07-04 21:46:35 +0530

    Project file uploaded

---
+++ result.py
@@ -0,0 +1,7 @@
+Python 3.6.7 (v3.6.7:6ec5cf24b7, Oct 20 2018, 13:35:33) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:/Users/PC/AppData/Local/Programs/Python/Python36/project.py ===
+Enter text python
+fd07d89db946b3c2bf8ad73f49c7830a
+>>> 
